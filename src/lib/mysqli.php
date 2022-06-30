29cec654957af8884dfa3c6f191c690ae11482b1
koji13098 <kojirosan135@gmail.com>
2022-07-01 03:12:41 +0900

    データベース接続に環境変数を使用

--- src/lib/mysqli.php
+++ src/lib/mysqli.php
@@ -1,8 +1,12 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
+$dotenv->load();
+
 function dbConnect(): mysqli
 {
-    $link = mysqli_connect('db', 'picker', 'pass', 'store');
+    $link = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], $_ENV['DB_DATABASE']);
     if (!$link) {
         echo 'Error: Failed to connect to store' . PHP_EOL;
         echo mysqli_connect_error() . PHP_EOL;
