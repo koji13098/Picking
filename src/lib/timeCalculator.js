704356d471b48c7c7d1ffe785970bf7d76f83a3f
koji13098 <kojirosan135@gmail.com>
2022-07-24 03:18:14 +0900

    表示の修正、即時関数化

--- src/lib/timeCalculator.js
+++ src/lib/timeCalculator.js
@@ -1,11 +1,39 @@
-const timeTable = document.getElementById("time-table");
-const breakTable = document.getElementById("break-table");
-const sumTime = document.getElementById("resultSum");
+(() => {
+
+  const timeTable = document.getElementById("time-table");
+  const sumTime = document.getElementById("resultSum");
+
+  function calculateTime() {
+    let startTime = document.querySelectorAll(".start-time");
+    let endTime = document.querySelectorAll(".end-time");
+    let breakTime = document.querySelectorAll(".break-time");
+    let result = document.querySelectorAll(".result");
+    let resultSum = 0;
+
+    for (let i = 0; i < timeTable.rows.length - 1; i++) {
+      if (startTime[i].value != '' || endTime[i].value != '') {
+        let sumMinute = parseInt(endTime[i].value.substr(3, 2)) - parseInt(startTime[i].value.substr(3, 2));
+        let sumHour = parseInt(endTime[i].value.substr(0, 2)) - parseInt(startTime[i].value.substr(0, 2));
+        sum = sumHour * 60 + sumMinute - breakTime[i].value;
+        result[i].value = sum;
+        resultSum += sum;
+      }
+    }
+
+    return resultSum;
+  }
+
+  document.getElementById("calculate").addEventListener("click", () => {
+    sumTime.value = calculateTime() + "分";
+  })
+
+})();
 
 function insertRow(tableId, row) {
-  console.log(tableId);
-  console.log(row);
   var tr = document.querySelector(row).cloneNode(true);
+  tr.querySelectorAll("input").forEach(input => {
+    input.value = '';
+  });
   document.getElementById(tableId).querySelector("tbody").appendChild(tr);
 }
 
@@ -15,31 +43,3 @@ function deleteRow(obj) {
     tr.parentNode.deleteRow(tr.sectionRowIndex);
   }
 }
-
-function calculateTime() {
-  let startTime = document.querySelectorAll(".start-time");
-  let endTime = document.querySelectorAll(".end-time");
-  let breakTime = document.querySelectorAll(".break-time");
-  let result = document.querySelectorAll(".result");
-  let resultSum = 0;
-
-  for (let i = 0; i < timeTable.rows.length - 1; i++) {
-    console.log(timeTable.rows.length);
-    console.log(startTime[i]);
-    console.log("start = " + startTime[i].value);
-    console.log("end = " + endTime[i].value);
-    let sumHour = parseInt(endTime[i].value.substr(0, 2)) - parseInt(startTime[i].value.substr(0, 2));
-    let sumMinute = parseInt(endTime[i].value.substr(3, 2)) - parseInt(startTime[i].value.substr(3, 2));
-    sum = sumHour * 60 + sumMinute - breakTime[i].value;
-    result[i].value = sum;
-    resultSum += sum;
-    console.log(sum);
-    console.log(resultSum);
-  }
-
-  return resultSum;
-}
-
-document.getElementById("calculate").addEventListener("click", () => {
-  sumTime.value = calculateTime();
-})
