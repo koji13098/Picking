1225e8213e2a653e77916f3a68bef08033adbffa
koji13098 <kojirosan135@gmail.com>
2022-07-26 04:14:55 +0900

    (fixed)MySQL対応 (fixed)MySQLの日本語対応

--- src/forum.php
+++ src/forum.php
@@ -1,10 +1,23 @@
 <?php
 
-date_default_timezone_set('Asia/Tokyo');
+require_once __DIR__ . '/lib/mysqli.php';
 
-function escape($str)
+function escape($str): string
 {
   return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+
+function getMessages($link): array
+{
+  $sql = 'SELECT id, text, timestamp FROM forum';
+  if (!($result = mysqli_query($link, $sql))) {
+    error_log("Error: " . mysqli_error($link));
+    return ['投稿一覧を取得できませんでした'];
+  }
+  while ($message = mysqli_fetch_assoc($result)) {
+    $messages[] = $message;
+  }
+  return $messages;
 }
 
 session_start();
@@ -12,25 +25,22 @@ session_start();
 $text = (string)filter_input(INPUT_POST, 'text');
 $token = (string)filter_input(INPUT_POST, 'token');
 
-$file = fopen('./forum.csv', 'a+b');
+$link = dbConnect();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && $text && sha1(session_id() === $token)) {
-  header("Location:forum.php");
-  flock($file, LOCK_EX);
-  fputcsv($file, [date('Y/m/d/H:i:s'), $text]);
-  rewind($file);
+  $sql = 'INSERT INTO forum (text) VALUES ("' . $text . '")';
+
+  if (!($result = mysqli_query($link, $sql))) {
+    error_log("Error: " . mysqli_error($link));
+  }
+
   session_regenerate_id(true);
+  header("Location:forum.php");
 }
 
-$rows = [];
-flock($file, LOCK_SH);
-while ($row = fgetcsv($file)) {
-  $rows[] = $row;
-}
-$rowsReversed = array_reverse($rows);
-flock($file, LOCK_UN);
-fclose($file);
+$messages = array_reverse(getMessages($link));
 
+mysqli_close($link);
 
 ?>
 
@@ -58,17 +68,17 @@ fclose($file);
   </div>
   <div class="forum-list">
     <div class="title">投稿一覧</div>
-    <?php if (empty($rowsReversed)) : ?>
+    <?php if (empty($messages)) : ?>
       <p>投稿はまだありません</p>
     <?php else : ?>
       <ul>
-        <?php foreach ($rowsReversed as $row) : ?>
+        <?php foreach ($messages as $message) : ?>
           <li>
-            <div class="forum-text">
-              <?= escape($row[1]) ?>
+            <div class="forum-header">
+              <?= escape($message['id']) ?>.&nbsp;<?= escape($message['timestamp']) ?>
             </div>
-            <div class="forum-date">
-              <?= escape($row[0]) ?>
+            <div class="forum-text">
+              <?= escape($message['text']) ?>
             </div>
           </li>
         <?php endforeach; ?>
